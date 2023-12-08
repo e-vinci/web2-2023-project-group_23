8dee4b0fa435aa9348addf5dd1758638e98b2b67
Muhammad-Haziq12 <muhammad.haziq@student.vinci.be>
2023-12-08 22:20:52 +0100

    upgrade sign-up page and some functions add

--- frontend/src/Components/Pages/Signup.js
+++ frontend/src/Components/Pages/Signup.js
@@ -71,6 +71,8 @@ function Signuppagefuntion() {
                   </div>
                   </div>
 
+                  
+
                   <div class="row mb-4">
                   
                   <div class="col">
@@ -88,9 +90,7 @@ function Signuppagefuntion() {
                 </div>
                   
 
-                 
-
-              
+                
                   <div class="form-check d-flex justify-content-center mb-5">
                     <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
                     <label class="form-check-label" for="form2Example3">
@@ -102,6 +102,7 @@ function Signuppagefuntion() {
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                   <input type="submit" id="registerButton" class="btn" value="Sign up" />
                   </div>
+                  <div id="errorContainer" class="alert alert-danger" role="alert"></div>
                 
                 </form>
 
@@ -129,6 +130,8 @@ function Signuppagefuntion() {
 
 function ad() {
   const registerBtn = document.querySelector('#registerButton')
+  const errorContainer = document.querySelector('#errorContainer');
+  
   registerBtn.addEventListener('click', async(e)=>{
     e.preventDefault();
     const username = document.querySelector('#username').value;
@@ -139,8 +142,43 @@ function ad() {
     const city = document.querySelector('#city').value;
     const zip = document.querySelector('#codezip').value;
     const adresse = `${street}, ${city}, ${zip}`;
+    const termsCheckbox = document.querySelector('#form2Example3c');
 
-  
+
+    // Réinitialiser les erreurs précédentes
+    errorContainer.innerHTML = '';
+    errorContainer.style.display = 'none';
+
+
+    // Validation des champs
+    const missingFields = [];
+    if (!username) missingFields.push('Username');
+    if (!email) missingFields.push('Email');
+    if (!password) missingFields.push('Password');
+
+
+    if (missingFields.length > 0) {
+      // Construire le message d'erreur
+      const errorMessage = `Veuillez remplir les champs suivants : ${missingFields.join(', ')}.`;
+
+      // Afficher le message d'erreur dans l'élément approprié
+      errorContainer.innerHTML = errorMessage;
+      errorContainer.style.display = 'block'; // Afficher le conteneur d'erreur
+
+      return;
+    }
+
+    
+    if (!termsCheckbox.checked) {
+      // Construire le message d'erreur
+
+      // Afficher le message d'erreur dans l'élément approprié
+      errorContainer.innerHTML = 'Veuillez accepter les conditions d\'utilisation.';
+      errorContainer.style.display = 'block'; // Afficher le conteneur d'erreur
+
+      return;
+    }
+
     const options = {
       method: 'POST',
       body: JSON.stringify({
@@ -164,205 +202,4 @@ function ad() {
     
   })
 }
-/*
-async function onRegister(e){
-  e.preventDefault();
-  const username = document.querySelector('#username').value;
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
-
-  const options = {
-    method: 'POST',
-    body: JSON.stringify({
-      username,
-      email,
-      password,
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
-  const response = await fetch('/api/auths/register', options);
-  if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
-  const authenticatedUser = await response.json();
-  console.log('Newly registered & authenticated user : ', authenticatedUser);
-  
-  Navigate('/');
-}
-
-*/
-/*
-
-function renderRegisterForm() {
-  const main = document.querySelector('main');
-  const form = document.createElement('form');
-  form.className = 'p-5';
-  const username = document.createElement('input');
-  username.type = 'text';
-  username.id = 'username';
-  username.placeholder = 'username';
-  username.required = true;
-  username.className = 'form-control mb-3';
-  const email = document.createElement('input');
-  email.type = 'text';
-  email.id = 'email';
-  email.placeholder = 'mail';
-  email.required = true;
-  email.className = 'form-control mb-3';
-  const password = document.createElement('input');
-  password.type = 'password';
-  password.id = 'password';
-  password.required = true;
-  password.placeholder = 'password';
-  password.className = 'form-control mb-3';
-  const submit = document.createElement('input');
-  submit.value = 'Register';
-  submit.type = 'submit';
-  submit.className = 'btn btn-danger';
-  form.appendChild(username);
-  form.appendChild(email);
-  form.appendChild(password);
-  form.appendChild(submit);
-  main.appendChild(form);
-  form.addEventListener('submit', onRegister);
-  
-}
-*/
-
-
-/*
-function Signuppagefuntion() {
-  const Signup = `
-  <section class="vh-100" style="background-color: #eee;">
-  <div class="container h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
-          <div class="card-body p-md-5">
-            <div class="row justify-content-center">
-              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign Up Form</p>
-
-                <form class="mx-1 mx-md-4">
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="text" id="username" class="form-control" placeholder="WILL BE USE TO SIGN IN "/>
-                      <label class="form-label" for="form3Example1c">Your Username</label>
-                    </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="email" id="mail" class="form-control" />
-                      <label class="form-label" for="form3Example3c">Your Email</label>
-          
-                    </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="passeword" class="form-control" />
-                      <label class="form-label" for="form3Example4c">Password</label>
-                    </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                    <div class="form-outline flex-fill mb-0">
-                      <input type="password" id="passeword" class="form-control" />
-                      <label class="form-label" for="form3Example4cd">Repeat your password</label>
-                    </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                  <i class="fas fa-mobile fa-lg me-3 fa-fw"></i>
-                  <div class="form-outline flex-fill mb-0">
-                    <input type="password" id="number" class="form-control" />
-                    <label class="form-label" for="form3Example4cd">Your phone number</label>
-                  </div>
-                  </div>
-
-                  <div class="d-flex flex-row align-items-center mb-4">
-                  <i class="fas fa-house fa-lg me-3 fa-fw"></i>
-                  <div class="form-outline flex-fill mb-0">
-                    <input type="password" id="adress" class="form-control" />
-                    <label class="form-label" for="form3Example4cd">Street Number</label>
-                  </div>
-                  </div>
-
-                  <div class="row mb-4">
-                  
-                  <div class="col">
-                    <div class="form-outline">
-                      <input type="text" id="adress" class="form-control input-custom" />
-                      <label class="form-label" for="form9Example3">City</label>
-                    </div>
-                  </div>
-                  <div class="col">
-                    <div class="form-outline">
-                      <input type="text" id="adress" class="form-control input-custom" />
-                      <label class="form-label" for="form9Example4">Zip</label>
-                    </div>
-                  </div>
-                </div>
-
-                  <div class="form-check d-flex justify-content-center mb-5">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-                    <label class="form-check-label" for="form2Example3">
-                      I agree all statements in <a href="#!">Terms of service</a>
-                    </label>
-                    
-                  </div>
-
-                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                  <input type="submit" class="btn btn-primary" value="Sign-Up" />
-                  </div>
-
-                </form>
-
-              </div>
-              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                  class="img-fluid" alt="Sample image">
-
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-    `;
-  const main = document.querySelector('main');
-  main.innerHTML = Signup;
-  const myform = <document.querySelector('form');>
-  const name = document.querySelector('#username');
-  const email = document.querySelector('#mail');
-  const password = document.querySelector('#passeword');
-  const phone = document.querySelector('#number');
-  const adresse = document.querySelector('#adress');
-
-  myform.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const userTobeCreated = {
-      name: name.value,
-      email: email.value,
-      password: password.value,
-      phone: phone.value,
-      adresse: adresse.value,
-    };
-    await usersignup(userTobeCreated);
-    Navigate('/');
-  });
-}
-*/
 export default signUpPage;
