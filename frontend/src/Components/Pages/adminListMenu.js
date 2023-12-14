8dcb7f737d59142647f7af8e386a6b4ee04ace8e
Pop <vasil.pop@student.vinci.be>
2023-12-14 13:58:48 +0100

    pageAdmin

--- frontend/src/Components/Pages/adminListMenu.js
+++ frontend/src/Components/Pages/adminListMenu.js
@@ -1,76 +1,89 @@
-import readAllMenus from '../../models/menus';
+import { readAllMenus, deleteOneMenu, updatePartiallyOneMenu } from '../../models/menus';
 
-const AdminPage = async () => {
-  const infos = await readAllMenus();
-  const items = infos;
-  let page = `<div class="PageAdminCss">
-  <h2>Les Menus</h2>`;
-
-  page += `
-    <table class="admin-table">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Price</th>
-          <th>Image</th>
-          <th>Supprimer</th>
-          <th>Modifier</th>
-        </tr>
-      </thead>
-      <tbody>
-  `;
-
-  items.forEach(element => {
-
-    page += `
-      <tr class="Admin">
-        <td>${element.type}</td>
-        <td>${element.title}</td>
-        <td>${element.description}
-        <td>${element.price}€</td>
-        <td>
-        <img src="${element.imagelink}" alt=""></td>
-        <td>
-          <a href="#">Supprimer</a>
-        </td>
-        <td>
-          <a href="#">Modifier</a>
-        </td>
-      </tr>
-    `;
-  });
-
-  page += `
-      </tbody>
-    </table>
-    <div class="FormulaireList">
-    <form id="menuForm">
-    <h2>Rajouter un Menu</h2>
-
-    <label for="type">Type:</label>
-    <input type="text" id="type" name="type" required>
-
-    <label for="title">Title:</label>
-    <input type="text" id="title" name="title" required>
-
-    <label for="description">Description:</label>
-    <textarea id="description" name="description" required></textarea>
-
-    <label for="price">Price:</label>
-    <input type="number" id="price" name="price" required>
-
-    <label for="image">Image URL (optional):</label>
-    <input type="text" id="image" name="image">
-
-    <button type="button" onclick="addMenu()">Ajouter Menu</button>
-  </form>
-  </div>
-  `;
-
+const ViewMenuPage = async () => {
   const main = document.querySelector('main');
-  main.innerHTML = page;
+  main.innerHTML = '<div id="menuWrapper"></div>';
+
+  const menuWrapper = document.querySelector('#menuWrapper');
+
+  const menus = await readAllMenus();
+  const menusAsHtmlTable = getHtmlMenuTableAsString(menus);
+
+  menuWrapper.innerHTML = menusAsHtmlTable;
+
+  attachEventListeners();
 };
 
-export default AdminPage ;
+function getHtmlMenuTableAsString(movies) {
+  if (menus?.length === undefined || menus.length === 0) {
+    return '<p class="p-5">No menu yet : (</p>';
+  }
+
+  const htmlMenuTable = `<div class="table-responsive p-5">
+  <table class="table">
+<thead>
+  <tr>
+    <th scope="col">Id</th>
+    <th scope="col">Title</th>
+    <th scope="col">Type</th>
+    <th scope="col">Description</th>
+    <th scope="col">Price</th>  
+    <th scope="col">ImageLink</th>  
+  </tr>
+</thead>
+<tbody>  
+  ${menus
+    .map(
+      (element) => `
+    <tr>
+      <td class="fw-bold text-info" contenteditable="true">${element.id}</td>
+      <td class="fw-bold text-info" contenteditable="true">${element.title}</td>
+      <td class="fw-bold text-info" contenteditable="true">${element.type}</td>
+      <td class="fw-bold text-info" contenteditable="true">${element.description}</td>
+      <td class="fw-bold text-info" contenteditable="true">${element.price}</td>
+      <td class="fw-bold text-info" contenteditable="true">${element.imageLink}</td>
+    
+        <button type="button" class="btn btn-info delete" data-element-id="${element.id}">Delete</button>
+      </td>
+      <td>
+        <button type="button" class="btn btn-info update" data-element-id="${element.id}">Save</button>
+      </td>
+    </tr>
+    `,
+    )
+    .join('')}
+  </tbody></table>`;
+
+  return htmlMenuTable;
+}
+
+function attachEventListeners() {
+  const MenuWrapper = document.querySelector('#menuWrapper');
+
+  menuWrapper.querySelectorAll('.delete').forEach((button) => {
+    button.addEventListener('click', async (e) => {
+      const { elementId } = e.target.dataset;
+      await deleteOneMenu(elementId);
+      ViewMenuPage();
+    });
+  });
+
+  menuWrapper.querySelectorAll('.update').forEach((button) => {
+    button.addEventListener('click', async (e) => {
+      const { elementId } = e.target.dataset;
+
+      const menuRow = e.target.parentElement.parentElement;
+      const newMenumData = {
+        title: menuRow.children[0].innerText,
+        type: menuRow.children[1].innerText,
+        description: menuRow.children[2].innerText,
+        price: Number.parseInt(menuRow.children[3].innerText,10),
+        imageLink: menuRow.children[4].innerText,
+      };
+      await updatePartiallyOneMenu(elementId, newMenumData);
+      ViewMenuPage();
+    });
+  });
+}
+
+export default ViewMenuPage;
